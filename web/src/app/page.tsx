d1386d7ebbe91c09b0df7237dc584044731bd7a2
mellcosta <m3licosta@gmail.com>
2023-05-28 19:57:14 +0100

    Finished Page

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
           href=""
           className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
         >
-          <div className="flex h-10 w-10 items-center justify-items-center rounded-full bg-gray-400">
+          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
             <User className="h-5 w-5 text-gray-500" />
           </div>
 
@@ -34,34 +34,34 @@ export default function Home() {
           <Image src={nlwLogo} alt="NLW Spacetime" />
 
           <div className="max-w-[420px] space-y-1">
-            <h1 className="text-5xl font-bold leading-tight text-gray-50">
+            <h1 className=" text-5xl font-bold leading-tight text-gray-50">
               Sua cápsula do tempo
             </h1>
             <p className="text-lg leading-relaxed">
               Colecione momentos marcantes da sua jornada e compartilhe (se
-              quiser) som o mundo!
+              quiser) com o mundo!
             </p>
           </div>
 
           <a
             href=""
-            className=" inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
+            className=" inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm font-bold uppercase leading-none text-black hover:bg-green-600"
           >
             CADASTRAR LEMBRANÇA
           </a>
+        </div>
 
-          {/* Copyright */}
-          <div className="text-sm leading-relaxed text-gray-200">
-            Feito com 💜 no NLW da{' '}
-            <a
-              href="https://rocketseat.com.br"
-              target="_blank"
-              rel="noreferrer"
-              className="underline hover:text-gray-100"
-            >
-              Rocketseat
-            </a>
-          </div>
+        {/* Copyright */}
+        <div className="text-sm leading-relaxed text-gray-200">
+          Feito com 💜 no NLW da{' '}
+          <a
+            href="https://rocketseat.com.br"
+            target="_blank"
+            rel="noreferrer"
+            className="underline hover:text-gray-100"
+          >
+            Rocketseat
+          </a>
         </div>
       </div>
 
