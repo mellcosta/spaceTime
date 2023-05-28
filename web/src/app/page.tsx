2adfa0ebb0af7f53fe7e39365134e1db9348126e
mellcosta <m3licosta@gmail.com>
2023-05-28 14:44:43 +0100

    Sign in

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,8 @@
+import { User } from 'lucide-react'
+import Image from 'next/image'
+
+import nlwLogo from '../assets/nlw-spacetime-logo'
+
 export default function Home() {
   return (
     <main className="grid min-h-screen grid-cols-2">
@@ -8,6 +13,23 @@ export default function Home() {
 
         {/* Stripes */}
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+
+        {/* Sign In */}
+        <a
+          href=""
+          className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
+        >
+          <div className="flex h-10 w-10 items-center justify-items-center rounded-full bg-gray-400">
+            <User className="h-5 w-5 text-gray-500" />
+          </div>
+
+          <p className="max-w-[140px] text-sm leading-snug">
+            <span className="underline">Crie sua conta</span> e salve suas
+            memórias!
+          </p>
+        </a>
+
+        {/* Hero */}
       </div>
 
       {/* Right */}
