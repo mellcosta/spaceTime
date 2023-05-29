b6c0f5403f7c114b26ac6c0201290ab3f46d6626
mellcosta <m3licosta@gmail.com>
2023-05-29 11:02:02 +0100

    Colors

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View className="bg-gray-950 flex-1 items-center justify-center ">
-      <Text className='text-zinc-50 text-2xl' >Hello World!</Text>
+      <Text className='text-gray-50 text-5xl font-bold' >Rocketseat</Text>
       <StatusBar style="light" translucent />
     </View>
   );
