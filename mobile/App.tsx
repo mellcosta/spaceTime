83d99c3ab4caf123da24e1b285ec7f639debf259
mellcosta <m3licosta@gmail.com>
2023-05-30 19:03:13 +0100

    Importar Fonts

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
+
+import { Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+import { useFonts } from 'expo-font'
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
   return (
-    <View className="bg-gray-950 flex-1 items-center justify-center ">
-      <Text className='text-gray-50 text-5xl font-bold' >Rocketseat</Text>
+    <View className="flex-1 items-center justify-center bg-gray-900">
+      <Text className="font-body text-5xl text-gray-50">Rocketseat</Text>
       <StatusBar style="light" translucent />
     </View>
-  );
+  )
 }
-
