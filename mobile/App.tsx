3b1628e099a0f0271cdf7e41ce4dac8b45bdebf2
mellcosta <m3licosta@gmail.com>
2023-05-30 19:56:51 +0100

    Stripes

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -7,6 +7,7 @@ import { useFonts } from 'expo-font'
 
 import bgBlur from './src/assets/bg-blur.png'
 import Stripes from './src/assets/stripes.svg'
+import { styled } from 'nativewind'
 
 export default function App() {
   const [hasLoadedFonts] = useFonts({
@@ -14,6 +15,8 @@ export default function App() {
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   })
+
+  const StyledStripes = styled(Stripes)
 
   if (!hasLoadedFonts) {
     return null
@@ -25,6 +28,8 @@ export default function App() {
       className="relative flex-1 items-center bg-gray-900"
       imageStyle={{ position: 'absolute', left: '-100%' }}
     >
+      <StyledStripes className="absloute left-2" />
+
       <StatusBar style="light" translucent />
     </ImageBackground>
   )
